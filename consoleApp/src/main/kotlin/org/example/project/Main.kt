3daa4d6877eded7e61706186bd2b40ec0e774b76
stuffacc <st141867@spbu.ru>
2026-07-24 22:23:39 +0300

    clear

---
+++ consoleApp/src/main/kotlin/org/example/project/Main.kt
@@ -0,0 +1,5 @@
+package org.example.org.example.project
+
+fun main() {
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,7 +32,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-include(":androidApp")
 include(":desktopApp")
 include(":shared")
-include(":webApp")
+include("consoleApp")
