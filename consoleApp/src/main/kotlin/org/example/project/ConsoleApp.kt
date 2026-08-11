c798b8ff2869c7c791045e7b71cc1759c0c7a086
stuffacc <st141867@spbu.ru>
2026-08-11 15:10:49 +0300

    CLI layer

--- consoleApp/src/main/kotlin/org/example/project/ConsoleApp.kt
+++ consoleApp/src/main/kotlin/org/example/project/ConsoleApp.kt
@@ -1,6 +1,6 @@
 package org.example.project
 
-import org.example.project.data.RandomValueGenerator
+import org.example.project.data.RandomValueGeneratorImpl
 import org.example.project.data.repository.GameRepositoryImpl
 import org.example.project.data.repository.PlayerRepositoryImpl
 
@@ -8,7 +8,7 @@ import org.example.project.data.repository.PlayerRepositoryImpl
 fun main() {
     val playerRepository = PlayerRepositoryImpl()
     val gameRepository = GameRepositoryImpl()
-    val randomValueGenerator = RandomValueGenerator()
+    val randomValueGenerator = RandomValueGeneratorImpl()
 
     val applicationController = ApplicationController(playerRepository, gameRepository, randomValueGenerator)
 
